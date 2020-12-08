bf7dd452414ce03f8db18d5716882c49a699afb8
Pavol Vargovcik <pavol.vargovcik@gmail.com>
2020-12-08 18:19:58 +0100

    delayPredicates

--- src/model/Afa.hs
+++ src/model/Afa.hs
@@ -2,12 +2,20 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Afa where
 
+import Data.Functor
 import Data.Array
-import Afa.Term.Mix (Term)
+import Afa.Term.Mix (Term(..))
 import Afa.Lib.Tree
+import Control.RecursionSchemes.Lens
+import Control.Monad.Trans
+import Control.Monad.Identity
+import Data.Traversable
+import Afa.Lib (listArray')
 
 data Afa terms states init = Afa
   { terms :: terms
@@ -19,3 +27,17 @@ data Afa terms states init = Afa
 type MixTermITree p = Tree (Term p Int) Int
 type AfaSwallowed p = Afa (Array Int (MixTermITree p)) (Array Int (MixTermITree p)) Int
 type AfaUnswallowed p = Afa (Array Int (Term p Int Int)) (Array Int Int) Int
+
+delayPredicates :: AfaUnswallowed p -> AfaUnswallowed p
+delayPredicates afa@Afa{terms, states} = afa
+  { terms = listArray'$ elems terms1 ++ terms2
+  , states = listArray'$ elems states ++ states2
+  }
+  where
+  stateCount = rangeSize (bounds states)
+  termCount = rangeSize (bounds terms)
+  ((terms1, terms2), states2) =
+    runIdentity$ runNoConsTFrom stateCount$ runNoConsTFrom termCount$
+      for terms$ \case
+        p@(Predicate _) -> (nocons p >>= lift . nocons) <&> State
+        x -> return x
