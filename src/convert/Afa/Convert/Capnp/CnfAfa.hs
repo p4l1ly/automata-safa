a306bf4e45ee5b264e71d828087637afa7c8de20
Pavol Vargovcik <pavol.vargovcik@gmail.com>
2020-12-11 11:33:28 +0100

    convert: CnfAfa -> capnp

--- src/convert/Afa/Convert/Capnp/CnfAfa.hs
+++ src/convert/Afa/Convert/Capnp/CnfAfa.hs
@@ -5,7 +5,7 @@ module Afa.Convert.Capnp.CnfAfa where
 import Data.Array
 import qualified Capnp
 import qualified Capnp.GenHelpers.ReExports.Data.Vector as V
-import qualified Capnp.Gen.Schema.CnfAfa.Pure as Schema
+import qualified Capnp.Gen.Afa.Model.CnfAfa.Pure as Schema
 import System.IO
 
 import Afa.Convert.CnfAfa (CnfAfa(..), Lit(..))
@@ -13,11 +13,11 @@ import Afa.Convert.CnfAfa (CnfAfa(..), Lit(..))
 hWriteCnfAfa :: CnfAfa -> Handle -> IO ()
 hWriteCnfAfa afa h = Capnp.hPutValue h$ serializeCnfAfa afa
 
-serializeCnfAfa :: CnfAfa -> Schema.CnfAfa
-serializeCnfAfa (CnfAfa states varCount clauses) = Schema.CnfAfa
+serializeCnfAfa :: CnfAfa -> Schema.Afa
+serializeCnfAfa (CnfAfa states varCount clauses) = Schema.Afa
   { Schema.variableCount = fromIntegral varCount
   , Schema.outputs = V.fromList$ map toSchemaLit$ elems states
-  , Schema.clauses = V.fromList$ map (V.fromList . map toSchemaLit)$ elems clauses
+  , Schema.clauses = V.fromList$ map (V.fromList . map toSchemaLit) clauses
   }
 
 toSchemaLit :: Lit -> Schema.Lit
