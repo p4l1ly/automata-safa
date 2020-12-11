e518449dbf1272861b3c007ea761b2f5cddf3e94
Pavol Vargovcik <pavol.vargovcik@gmail.com>
2020-12-11 11:31:04 +0100

    convert: BoolAfa -> capnp

--- src/convert/Afa/Convert/Capnp/Afa.hs
+++ src/convert/Afa/Convert/Capnp/Afa.hs
@@ -1,52 +1,82 @@
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE NamedFieldPuns #-}
 
 module Afa.Convert.Capnp.Afa where
 
+import qualified Data.List.NonEmpty as NE
 import Data.Array
 import qualified Capnp
 import qualified Capnp.GenHelpers.ReExports.Data.Vector as V
-import Capnp.Gen.Schema.Afa.Pure
+import qualified Capnp.Gen.Afa.Model.Succinct.Pure as AfaC
+import qualified Capnp.Gen.Afa.Model.Term.Pure as TermC
 import System.IO
-import qualified Afa
-import qualified Afa.Term.TreeF as T
-import qualified Afa.Term.TreeFBase as TB
-import Data.Functor.Foldable (cata)
+import qualified Data.HashSet as HS
+import Data.Hashable
+import Data.Functor.Const
+import Data.Monoid (Endo(..))
 
-hReadAfa :: Handle -> IO Afa.Afa
+import Afa.Lib (listArray')
+import Afa
+import Afa.Bool
+import qualified Afa.Term.Bool as BTerm
+import qualified Afa.Term.Mix as MTerm
+
+hReadAfa :: Handle -> IO (BoolAfaUnswallowed Int)
 hReadAfa h = deserializeAfa <$> Capnp.hGetValue h Capnp.defaultLimit
 
-hWriteAfa :: Afa.Afa -> Handle -> IO ()
+hWriteAfa :: BoolAfaUnswallowed Int -> Handle -> IO ()
 hWriteAfa afa h = Capnp.hPutValue h$ serializeAfa afa
 
-deserializeAfa :: Afa -> Afa.Afa
-deserializeAfa Afa{terms, states} = Afa.Afa
-  { Afa.terms = listArray (0, length terms - 1)$ map deserializeTerm$ V.toList terms
-  , Afa.states = listArray (0, length states - 1)$ map fromIntegral$ V.toList states
+deserializeAfa :: AfaC.BoolAfa -> BoolAfaUnswallowed Int
+deserializeAfa (AfaC.BoolAfa aterms mterms states _) = BoolAfa
+  (listArray'$ map deserializeBTerm$ V.toList aterms)
+  ( Afa
+      (listArray'$ map deserializeMTerm$ V.toList mterms)
+      (listArray'$ map fromIntegral$ V.toList states)
+      0
+  )
+
+deserializeBTerm :: TermC.BoolTerm11 -> BTerm.Term Int Int
+deserializeBTerm TermC.BoolTerm11'litTrue = BTerm.LTrue
+deserializeBTerm TermC.BoolTerm11'litFalse = BTerm.LFalse
+deserializeBTerm (TermC.BoolTerm11'predicate p) = BTerm.Predicate$ fromIntegral p
+deserializeBTerm (TermC.BoolTerm11'not x) = BTerm.Not$ fromIntegral x
+deserializeBTerm (TermC.BoolTerm11'or xs) = BTerm.Or$ iVecToNe xs
+deserializeBTerm (TermC.BoolTerm11'and xs) = BTerm.And$ iVecToNe xs
+
+deserializeMTerm :: TermC.PredicateQTerm111 -> MTerm.Term Int Int Int
+deserializeMTerm TermC.PredicateQTerm111'litTrue = MTerm.LTrue
+deserializeMTerm (TermC.PredicateQTerm111'predicate p) = MTerm.Predicate$ fromIntegral p
+deserializeMTerm (TermC.PredicateQTerm111'state q) = MTerm.State$ fromIntegral q
+deserializeMTerm (TermC.PredicateQTerm111'or xs) = MTerm.Or$ iVecToNe xs
+deserializeMTerm (TermC.PredicateQTerm111'and xs) = MTerm.And$ iVecToNe xs
+
+serializeAfa :: BoolAfaUnswallowed Int -> AfaC.BoolAfa
+serializeAfa (BoolAfa bterms (Afa mterms states 0)) = AfaC.BoolAfa
+  { AfaC.aterms = V.fromList$ map serializeBTerm$ elems bterms
+  , AfaC.mterms = V.fromList$ map serializeMTerm$ elems mterms
+  , AfaC.states = V.fromList$ map fromIntegral$ elems states
+  , AfaC.varCount = fromIntegral$ varCount bterms
   }
 
-deserializeTerm :: Term -> T.Term
-deserializeTerm Term'litTrue = T.LTrue
-deserializeTerm Term'litFalse = T.LFalse
-deserializeTerm (Term'var i) = T.Var$ fromIntegral i
-deserializeTerm (Term'state i) = T.State$ fromIntegral i
-deserializeTerm (Term'ref i) = T.Ref$ fromIntegral i
-deserializeTerm (Term'and xs) = T.And$ map deserializeTerm$ V.toList xs
-deserializeTerm (Term'or xs) = T.Or$ map deserializeTerm$ V.toList xs
-deserializeTerm (Term'not x) = T.Not$ deserializeTerm x
+-- TODO multifold, multitraverse, multimap
+varCount :: (Traversable f, Eq p, Hashable p) => f (BTerm.Term p t) -> Int
+varCount = HS.size . (`appEndo` HS.empty) . getConst .
+  traverse (BTerm.modChilds BTerm.pureChildMod{ BTerm.lP = Const . Endo . HS.insert })
 
-serializeAfa :: Afa.Afa -> Afa
-serializeAfa (Afa.Afa terms states) = Afa
-  { terms = V.fromList$ map (cata serializeTerm)$ elems terms
-  , states = V.fromList$ map fromIntegral$ elems states
-  }
+serializeBTerm :: BTerm.Term Int Int -> TermC.BoolTerm11
+serializeBTerm BTerm.LTrue = TermC.BoolTerm11'litTrue
+serializeBTerm BTerm.LFalse = TermC.BoolTerm11'litFalse
+serializeBTerm (BTerm.Predicate p) = TermC.BoolTerm11'predicate$ fromIntegral p
+serializeBTerm (BTerm.Not x) = TermC.BoolTerm11'not$ fromIntegral x
+serializeBTerm (BTerm.Or xs) = TermC.BoolTerm11'or$ iNeToVec xs
+serializeBTerm (BTerm.And xs) = TermC.BoolTerm11'and$ iNeToVec xs
 
-serializeTerm :: TB.Term Term -> Term
-serializeTerm TB.LTrue = Term'litTrue
-serializeTerm TB.LFalse = Term'litFalse
-serializeTerm (TB.Var i) = Term'var$ fromIntegral i
-serializeTerm (TB.State i) = Term'state$ fromIntegral i
-serializeTerm (TB.Ref i) = Term'ref$ fromIntegral i
-serializeTerm (TB.And xs) = Term'and$ V.fromList xs
-serializeTerm (TB.Or xs) = Term'or$ V.fromList xs
-serializeTerm (TB.Not x) = Term'not x
+serializeMTerm :: MTerm.Term Int Int Int -> TermC.PredicateQTerm111
+serializeMTerm MTerm.LTrue = TermC.PredicateQTerm111'litTrue
+serializeMTerm (MTerm.Predicate p) = TermC.PredicateQTerm111'predicate$ fromIntegral p
+serializeMTerm (MTerm.State q) = TermC.PredicateQTerm111'state$ fromIntegral q
+serializeMTerm (MTerm.Or xs) = TermC.PredicateQTerm111'or$ iNeToVec xs
+serializeMTerm (MTerm.And xs) = TermC.PredicateQTerm111'and$ iNeToVec xs
+
+iNeToVec = V.fromList . map fromIntegral . NE.toList
+iVecToNe = NE.fromList . map fromIntegral . V.toList
