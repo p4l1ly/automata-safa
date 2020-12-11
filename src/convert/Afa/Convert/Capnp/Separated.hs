5450dd7b9e181442593c64b3d9bb2fc8c026a62d
Pavol Vargovcik <pavol.vargovcik@gmail.com>
2020-12-11 12:01:18 +0100

    convert: Separated -> capnp

--- src/convert/Afa/Convert/Capnp/Separated.hs
+++ src/convert/Afa/Convert/Capnp/Separated.hs
@@ -3,38 +3,40 @@
 
 module Afa.Convert.Capnp.Separated where
 
+import Data.Functor
 import Control.Arrow
+import Data.Void
 import Data.Array
-import qualified Capnp.Gen.Schema.SeparatedAfa.Pure as Schema
+import qualified Capnp.Gen.Afa.Model.Separated.Pure as AfaC
+import qualified Capnp.Gen.Afa.Model.Term.Pure as TermC
 import qualified Capnp.GenHelpers.ReExports.Data.Vector as V
-import Data.Functor.Tree (pattern BLeaf, pattern BNode, TreeBase)
-import Data.Functor.Foldable (cata)
 import qualified Capnp
 import System.IO
 
-import Afa.Convert.Separated (SeparatedAfa(..), QTerm(..), ATerm(..))
+import Afa.Convert.Separated.Model (Afa(..))
+import Afa.Convert.Capnp.Afa (serializeBTerm, varCount, iNeToVec)
+import Afa.Lib (listArray')
+import qualified Afa.Term.Bool as BTerm
+import qualified Afa.Term.Mix as MTerm
 
-hWrite :: Int -> SeparatedAfa -> Handle -> IO ()
-hWrite varCount afa h = Capnp.hPutValue h$ serialize varCount afa
+hWrite :: Afa Int -> Handle -> IO ()
+hWrite afa h = Capnp.hPutValue h$ serializeAfa afa
 
-serialize :: Int -> SeparatedAfa -> Schema.SeparatedAfa
-serialize varCount SeparatedAfa{qterms, aterms, states} = Schema.SeparatedAfa
-  { Schema.qterms = V.fromList$ map (cata serializeQTerm)$ elems qterms
-  , Schema.aterms = V.fromList$ map (cata serializeATerm)$ elems aterms
-  , Schema.states = V.fromList$ flip map (elems states)$
-      V.fromList . map (uncurry Schema.Conjunct . (fromIntegral *** fromIntegral))
-  , Schema.variableCount = fromIntegral varCount
+serializeAfa :: Afa Int -> AfaC.BoolAfa
+serializeAfa (Afa aterms qterms states 0) = AfaC.BoolAfa
+  { AfaC.aterms = V.fromList$ map serializeBTerm$ elems aterms
+  , AfaC.varCount = fromIntegral$ varCount aterms
+  , AfaC.qterms = V.fromList$ map serializeQTerm$ elems qterms
+  , AfaC.states = V.fromList$ elems states <&>
+      V.fromList . map (uncurry AfaC.Conjunct11 . (toMaybe1 *** toMaybe1))
   }
 
-serializeQTerm :: TreeBase QTerm Int Schema.QTerm -> Schema.QTerm
-serializeQTerm (BLeaf i) = Schema.QTerm'ref$ fromIntegral i
-serializeQTerm (BNode (QState i)) = Schema.QTerm'state$ fromIntegral i
-serializeQTerm (BNode (QAnd xs)) = Schema.QTerm'and$ V.fromList xs
-serializeQTerm (BNode (QOr xs)) = Schema.QTerm'or$ V.fromList xs
+toMaybe1 :: Maybe Int -> AfaC.Maybe1
+toMaybe1 Nothing = AfaC.Maybe1'nothing
+toMaybe1 (Just a) = AfaC.Maybe1'just$ fromIntegral a
 
-serializeATerm :: TreeBase ATerm Int Schema.ATerm -> Schema.ATerm
-serializeATerm (BLeaf i) = Schema.ATerm'ref$ fromIntegral i
-serializeATerm (BNode (AVar i)) = Schema.ATerm'var$ fromIntegral i
-serializeATerm (BNode (AAnd xs)) = Schema.ATerm'and$ V.fromList xs
-serializeATerm (BNode (AOr xs)) = Schema.ATerm'or$ V.fromList xs
-serializeATerm (BNode (ANot x)) = Schema.ATerm'not x
+serializeQTerm :: MTerm.Term Void Int Int -> TermC.QTerm11
+serializeQTerm MTerm.LTrue = TermC.QTerm11'litTrue
+serializeQTerm (MTerm.State q) = TermC.QTerm11'state$ fromIntegral q
+serializeQTerm (MTerm.Or xs) = TermC.QTerm11'or$ iNeToVec xs
+serializeQTerm (MTerm.And xs) = TermC.QTerm11'and$ iNeToVec xs
