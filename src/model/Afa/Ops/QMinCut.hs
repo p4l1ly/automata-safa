fbe724c16a1aea4e80e25057bc0a6352cf406fd6
Pavol Vargovcik <pavol.vargovcik@gmail.com>
2020-12-20 21:21:16 +0100

    QMinCut

--- src/model/Afa/Ops/QMinCut.hs
+++ src/model/Afa/Ops/QMinCut.hs
@@ -3,13 +3,12 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TupleSections #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE ViewPatterns #-}
 
 module Afa.Ops.QMinCut where
 
-import Debug.Trace
-
+import Data.List
+import Data.Maybe
+import Control.Monad
 import Control.Monad.Trans
 import Control.Monad.Trans.Except
 import Data.Monoid (Any(..), Endo(..))
@@ -48,52 +47,95 @@ instance Semigroup Path where
   Visited _ <> _ = Blind
   _ <> x = x
 
-paths :: Foldable f => Array Int (f Int) -> [Int] -> Array Int Bool
-paths nodes sources = runST action
+
+maxFlow :: Foldable f => Array Int (f Int) -> [Int] -> [Int] -> Array Int Bool
+maxFlow nodes sources sinks = runST action
   where
   action :: forall s. ST s (Array Int Bool)
   action = do
-    blockersM <- newArray @(STArray s) (bounds nodes) False
+    residualGraphM <- newArray @(STArray s) (bounds nodes) Nothing
     let getPath = do
-          blockers <- unsafeFreeze blockersM
+          residualGraph <- unsafeFreeze residualGraphM
           arr <- newArray @(STArray s) (bounds nodes) Unvisited
           for_ sources$ \src -> writeArray arr src$ Visited$ Endo id
           runExceptT$ for_ sources$
-            dfs (traversal blockers arr)$ LiftArray arr
-        subtractPath ixs = for_ ixs$ \ix -> writeArray blockersM ix True
+            dfs (traversal residualGraph arr)$ LiftArray arr
+        subtractPath ixs = do
+          writeArray residualGraphM (head ixs)$ Just Nothing
+          for_ (zip (tail ixs) ixs)$ \(next, this) ->
+            if next < this
+            then writeArray residualGraphM next (Just$ Just this)
+            else writeArray residualGraphM this Nothing
         rec = getPath >>= \case Left p -> subtractPath p >> rec; _ -> return ()
     rec
-    unsafeFreeze blockersM
+    fmap isJust <$> unsafeFreeze residualGraphM
 
-  traversal blockers arr rec = \case
-    (Visited pp, i)
-      | blockers!i -> return ()
-      | null node -> throwE$ pp `appEndo` [i]
-      | otherwise -> do
-          lift$ writeArray arr i Blind
-          for_ node$ rec . (Visited$ pp <> Endo (i:),)
-      where node = nodes!i
+  traversal residualGraph arr rec = \case
+    (Visited pp, i) -> do
+      lift$ writeArray arr i Blind
+      case residualGraph!i of
+        Just (Just back) -> for_ (nodes!back) (recBackDown back) >> rec' back
+        Just _ -> return ()
+        _ | sinkFlags!i -> throwE$ pp `appEndo` [i]
+          | otherwise -> for_ (nodes!i) recDown
+      where
+      rec' = rec . (Visited$ pp <> Endo (i:),)
+      recDown j = unless (sourceFlags!j)$ rec' j
+      recBackDown back j = unless (sourceFlags!j)$ rec'' j
+        where rec'' = rec . (Visited$ pp <> Endo (\xs -> i:back:xs),)
     _ -> return ()
 
-minCut :: Traversable f => Array Int (f Int) -> [Int] -> [Int]
-minCut nodes sources = runST action where
+  sinkFlags = accumArray (\_ _ -> True) False (bounds nodes)$ map (, ()) sinks
+  sourceFlags = accumArray (\_ _ -> True) False (bounds nodes)$ map (, ()) sources
+
+
+minCut :: Traversable f => Array Int (f Int) -> [Int] -> [Int] -> [Int]
+minCut nodes sources sinks = runST action where
   action :: forall s. ST s [Int]
   action = do
-    let (traceShowId -> marks) = paths nodes sources
-    let (traceShowId -> marks10) = ixedNodes & cataScan (_2 . traversed) %~ \(i, fb) ->
-          let undermined = all snd fb && not (null fb)
-              mark = marks!i
-          in (not undermined, mark || undermined)
-    let marks1 = fmap fst marks10
-    let setter rec = \case
-          (Any False, _) -> pure False
-          (Any True, i) -> if marks1!i
-            then pure True
-            else True <$ for_ (nodes!i) (rec . (Any True,))
+    let marks = maxFlow nodes sources sinks
+    let marks1 = ixedNodes & cataScan (_2 . traversed) %~ \(i, fb) ->
+          marks!i || or fb && not (sinkFlags!i)
 
-    marks2Init :: STArray s Int Any <- unsafeThaw$
-      accumArray (\_ _ -> Any True) (Any False) (bounds nodes)$ map (, ()) sources
+    let setter rec = \(g, i) -> let overmined = marks!i || getAny g in
+          overmined <$ for_ (nodes!i) (rec . (Any overmined,))
+    marks2Init :: STArray s Int Any <- newArray (bounds nodes) (Any False)
     marks2M :: STArray s Int Bool <- marks2Init & anaScanT2 setter return
-    trueIndices . elems <$> unsafeFreeze marks2M
+    marks2 <- unsafeFreeze marks2M
+
+    if all (marks1!) sinks  -- topmost min cut: all (marks2!) sources
+      then return sinks  -- topmost min cut: return sources
+      else do
+        let newSources = map head$ group$ sort
+              [ j
+              | (i, node) <- elems ixedNodes
+              , area!i && not (marks2!i)
+              , j <- toList node
+              , marks2!j && not (sourceFlags!j)
+              ]
+        let newSinks = map head$ group$ sort
+              [ i
+              | (i, node) <- elems ixedNodes
+              , marks1!i && not (sinkFlags!i)
+              , any (\j -> area!j && not (marks1!j)) node
+              ]
+        let sources' = filter (marks2!) sources ++ newSources
+        let sinks' = filter (marks1!) sinks ++ newSinks
+        return$ minCut nodes sources' sinks'
 
   ixedNodes = listArray (bounds nodes)$ zip [0..] (elems nodes)
+
+  sinkFlags = accumArray (\_ _ -> True) False (bounds nodes)$ map (, ()) sinks
+  sourceFlags = accumArray (\_ _ -> True) False (bounds nodes)$ map (, ()) sources
+
+  aboveSinks = ixedNodes & cataScan (_2 . traversed) %~ \(i, fb) -> sinkFlags!i || or fb
+  underSources = runST action where
+    action :: forall s. ST s (Array Int Bool)
+    action = do
+      let setter rec = \(g, i) -> let overmined = sourceFlags!i || getAny g in
+            overmined <$ for_ (nodes!i) (rec . (Any overmined,))
+      marks2Init :: STArray s Int Any <- newArray (bounds nodes) (Any False)
+      marks2M :: STArray s Int Bool <- marks2Init & anaScanT2 setter return
+      unsafeFreeze marks2M
+
+  area = listArray (bounds nodes)$ zipWith (&&) (elems aboveSinks) (elems underSources)
--- test/Test/Afa/QMinCut.hs
+++ test/Test/Afa/QMinCut.hs
@@ -10,10 +10,11 @@ import Afa.Ops.QMinCut
 tests =
   [ "Afa.QMinCut" ~: do
       putStrLn ""
-      print$ minCut dag0 [7, 8, 9]
-      -- print$ minCut dag1 [7, 8, 9]
-      -- print$ minCut dag2 [7, 8, 9]
-      -- print$ minCut dag3 [10, 11, 9]
+      print$ minCut dag0 [7, 8, 9] [0, 1, 2]
+      print$ minCut dag1 [7, 8, 9] [0, 1, 2]
+      print$ minCut dag2 [7, 8, 9] [0, 1, 2]
+      print$ minCut dag3 [10, 11, 9] [0, 1, 2]
+      print$ minCut dag4 [8, 9, 10, 11] [0, 1, 2, 3]
   ]
 
 dag0 :: Array Int [Int]
@@ -51,7 +52,7 @@ dag2 = listArray'$ reverse
   , {- 7 -} [5]
   , {- 6 -} [4, 1]
   , {- 5 -} [3, 4]
-  , {- 4 -} [1, 2]
+  , {- 4 -} [2, 1]
   , {- 3 -} [0, 1]
   , {- 2 -} []
   , {- 1 -} []
@@ -69,6 +70,22 @@ dag3 = listArray'$ reverse
   , {-  5 -} [3]
   , {-  4 -} [1, 2]
   , {-  3 -} [0, 1]
+  , {-  2 -} []
+  , {-  1 -} []
+  , {-  0 -} []
+  ]
+
+dag4 :: Array Int [Int]
+dag4 = listArray'$ reverse
+  [ {- 11 -} [7]
+  , {- 10 -} [5, 7]
+  , {-  9 -} [5, 6]
+  , {-  8 -} [6]
+  , {-  7 -} [3, 4, 5]
+  , {-  6 -} [0, 1]
+  , {-  5 -} [1, 2]
+  , {-  4 -} [2, 3]
+  , {-  3 -} []
   , {-  2 -} []
   , {-  1 -} []
   , {-  0 -} []
