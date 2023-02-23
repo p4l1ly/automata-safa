2e35d9f09adb3218f100969594aba625490581da
jurajsic <juraj.sic@outlook.com>
2023-02-23 18:29:06 +0100

    emp converter can work also with incl

--- convert-one-emp.sh
+++ convert-one-emp.sh
@@ -15,7 +15,7 @@ mkdir $DANTONI/regexNfas
 
 WORKDIR=$(realpath "$1")
 GEN_AUT_DIR="$WORKDIR/gen_aut"
-rm -rf "$GEN_AUT_DIR"
+# rm -rf "$GEN_AUT_DIR"
 rm "$WORKDIR/result.emp"
 LOGFILE="$WORKDIR/log"
 rm "$LOGFILE"
@@ -33,11 +33,16 @@ while IFS= read -r line; do
 
     java -jar $BRICSREG $GEN_AUT_DIR/$name.range16nfa <<< "$regex" >>$LOGFILE 2>&1
     $LTLE_TO_AFA range16ToPretty < $GEN_AUT_DIR/$name.range16nfa > $GEN_AUT_DIR/$name.afa 2>>$LOGFILE
-    
+
     echo "@NFA-bits" > $GEN_AUT_DIR/$name.mata
     $LTLE_TO_AFA prettyToSeparatedMata < $GEN_AUT_DIR/$name.afa >> $GEN_AUT_DIR/$name.mata 2>>$LOGFILE
     echo "@NFA-bits" > $GEN_AUT_DIR/"$name"_mona.mata
     $LTLE_TO_AFA prettyToSeparatedDnfMata < $GEN_AUT_DIR/$name.afa >> $GEN_AUT_DIR/"$name"_mona.mata 2>>$LOGFILE
+
+  elif grep '^load_automaton' <<< "$line" > /dev/null; then 
+    name=$(sed -r 's/^load_automaton //' <<< "$line")
+    echo "load_automaton $name" >> "$WORKDIR/result.emp"
+
   elif grep '=' <<< "$line" > /dev/null; then
     name=$(sed -r 's/^([^ ]+) .*/\1/' <<< "$line")
     operator=$(sed -r 's/^[^ ]+ = \(([^ ]+) .*/\1/' <<< "$line")
@@ -65,5 +70,13 @@ while IFS= read -r line; do
     # export Afasat=true
     # export Mata=true
     # zsh "$CONVERT_FOR_CHECKERS" $WORKDIR/result.afa >>$LOGFILE 2>&1
+  elif grep '^incl' <<< "$line" > /dev/null; then
+    echo "$line" > "$WORKDIR/result.emp"
+
+    operands=($(sed -r 's/^incl //' <<< "$line"))
+    operands2=(${${operands[@]/%/.afa}/#/$GEN_AUT_DIR/})
+
+    $LTLE_TO_AFA negate < ${operands2[2]} > $GEN_AUT_DIR/${operands[2]}_neg.afa 2>>$LOGFILE
+    $LTLE_TO_AFA and ${operands2[1]} $GEN_AUT_DIR/${operands[2]}_neg.afa > $WORKDIR/result.afa 2>>$LOGFILE
   fi
 done
