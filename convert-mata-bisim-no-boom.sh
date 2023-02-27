121a4ddef996f0f83e14bcfc9c0696862f8178a1
jurajsic <juraj.sic@outlook.com>
2023-02-27 12:55:02 +0100

    mata/bisim converter that does no-boom separation

---
+++ convert-mata-bisim-no-boom.sh
@@ -0,0 +1,63 @@
+LTLE_TO_AFA=ltle-to-afa
+AUTOMATA_SAFA_ONE=automata-safa-one
+SMVTOAIG=~/aiger/smvtoaig
+SEPARATED=~/automata-safa-capnp/schema/Afa/Model/Separated.capnp
+
+if [ -z $1 ]; then
+  echo "path argument (to directory containing .afa files or to some .afa file) expected" >&2
+  exit 1
+fi
+
+afa_names="*"
+if [[ ! -z $2 ]]; then
+  afa_names="$2"
+fi
+
+if [ -f $1 ]; then
+  echo $1
+  let NUM_OF_INPUTS=1
+else
+  TMP=$(find $1 -name "$afa_names.afa" | wc -l)
+  let NUM_OF_INPUTS=TMP
+fi 
+
+let j=1
+if [ -f $1 ]; then
+  echo $1
+else
+  find $1 -name "$afa_names.afa"
+fi | while read -r fAfa; do
+  f=${fAfa%????}
+  echo "Processing $j/$NUM_OF_INPUTS: $f" >&2
+  let j++
+  ${Mata:-false} && {
+    echo "Transforming to .mata"
+    echo "@AFA-bits" > $f.mata
+
+    # TODO: problem, without boomSeparate, states are not in DNF
+    $AUTOMATA_SAFA_ONE isSeparated < $f.afa
+    if [ $? -eq 0 ]; then
+      $AUTOMATA_SAFA_ONE share < $f.afa | $AUTOMATA_SAFA_ONE removeFinalsHind | $AUTOMATA_SAFA_ONE unshare | $AUTOMATA_SAFA_ONE initToDnf | $LTLE_TO_AFA prettyToMachete >> $f.mata 
+    else
+      $AUTOMATA_SAFA_ONE share < $f.afa | $AUTOMATA_SAFA_ONE delaySymbolsLowest | $AUTOMATA_SAFA_ONE removeFinalsHind | $AUTOMATA_SAFA_ONE unshare | $AUTOMATA_SAFA_ONE initToDnf | $LTLE_TO_AFA prettyToMachete >> $f.mata 
+    fi
+  }
+  ${Bisim:-false} && {
+    echo "Transforming to .bisim"
+    TMP_DIR=$(mktemp -d)
+
+    $AUTOMATA_SAFA_ONE isSeparated < $f.afa
+    if [ $? -eq 0 ]; then
+      $AUTOMATA_SAFA_ONE share < $f.afa | $AUTOMATA_SAFA_ONE removeFinalsHind > "$TMP_DIR/0"
+    else
+      $AUTOMATA_SAFA_ONE share < $f.afa | $AUTOMATA_SAFA_ONE delaySymbolsLowest | $AUTOMATA_SAFA_ONE removeFinalsHind > "$TMP_DIR/0"
+    fi
+
+    echo "@kInitialFormula: s0
+@kFinalFormula: !s0
+@s0: kFalse" > "$TMP_DIR/1"
+
+    $LTLE_TO_AFA eqBisim "$TMP_DIR/0" "$TMP_DIR/1" | capnp convert binary:text $SEPARATED TwoBoolAfas | capnp convert text:binary $SEPARATED TwoBoolAfas > $f.bisim
+    rm -rf $TMP_DIR
+  }
+done
